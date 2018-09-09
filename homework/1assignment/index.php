682777e2a9aeff47f78e01f5c60cacd19c920c2f
John Shimun <ymar-eshai-shimun@csumb.edu>
2018-09-09 19:15:57 +0000

    added css precendence testing

---
+++ homework/1assignment/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    
+    <!-- -- this is the head, all JS and styles goes into head>
+    <head>
+        <meta charset= "utf-8" />
+        <title> Juan Doe: Personal Wesite </title>
+    </head>
+    <body>
+        <header>
+            <h1> </h1>
+        </header>
+      
+        <nav>
+            <hr width="50%" />
+            <a href="index.html">Home</a>
+            <a href="about.html">About</a>
+            <a href="contact.html">Contact</a>
+        </nav>
+         </br> </br>
+         
+         <main>
+             
+             <firgure>
+                 
+             </figure>
+             
+             <div>
+             
+            <div>
+        </main>
+                
+                 
+        
+         <!-- -- this is the footer, the footer goes inside the body, but its optional -->
+    <footer>
+         <hr>
+      
+        </hr>
+    </footer>
+
+    </body>
+</html>
---
+++ projects/personalportfolio/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+    
+  
+    <head>
+        <meta charset= "utf-8" />
+        <title><h1>Juan Doe: Personal Wesite </h1> </title>
+    </head>
+    <body>
+        <header>
+            <h1> Juan Doe</h1>
+        </header>
+      
+        <nav>
+            <hr width="50%" />
+            <a href="index.html">Home</a>
+            <a href="about.html">About</a>
+            <a href="contact.html">Contact</a>
+        </nav>
+         </br> </br>
+         
+         <main>
+             
+             <firgure>
+                 <img scr="/366/projects/personalportfolio/img/juan_doe.png" alt="Picture of Juan Doe"/>
+             </figure>
+             
+             <div>
+                 Hello!<br/>
+                 <p> Thank you for visiting my Proffesional Portfolio Website<p/>
+                 <p> I am a Software Engineer and have been recently hired at the Acme Corperation.</p>
+                 <p> Feel free to contact me! </P>
+                 <br /> <br />
+                 <em>"With ordinary talent and extraordinary
+                 <strong> perserverance </strong>, all things are attainable" </em> <br/>
+                 ~Thomas F. Buxton
+            <div>
+        </main>
+                
+                 
+        
+         <!-- -- this is the footer, the footer goes inside the body, but its optional -->
+    <footer>
+         <hr>
+         Internet Prgramming 2018&copy; Shimun<br/>
+         <strong> Disclamer: </strong>  The information in this webpage is fictitious. <br/>
+         it is used for class purpouse only
+        </hr>
+    </footer>
+
+    </body>
+</html>
