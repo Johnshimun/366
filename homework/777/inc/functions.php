65c8a0dfac366024bd8a61733cb4af8425b8863e
John Shimun <ymar-eshai-shimun@csumb.edu>
2018-09-19 15:54:37 +0000

    added 777 line 46 not working

---
+++ homework/777/inc/functions.php
@@ -0,0 +1,12 @@
+<?php
+/*function displayPoints($randomValue1, $randomValue2, $randomValue3) 
+ function displaySymbol($randomValue)
+ include 'inc/functions.php';
+ function play() {
+          for ($i=1; $i<4; $i++){
+            ${"randomValue" . $i } = rand(0,2);
+            displaySymbol(${"randomValue" . $i} );
+            displayPoints($randomValue1, $randomValue2, $randomValue3);
+*/
+
+?>
---
+++ practice/rps.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title> RPS </title>
+    <style type="text/css">
+        body {
+            background-color: black;
+            color: white;
+            text-align: center;
+        }
+
+        .row {
+            display: flex;
+            justify-content: center;
+        }
+
+        .col {
+            text-align: center;
+            margin: 0 70px;
+        }
+
+        .matchWinner {
+            background-color: yellow;
+            margin: 0 70px;
+        }
+
+        #finalWinner {
+            margin: 0 auto;
+            width: 500px;
+            text-align: center;
+        }
+        
+        hr {
+            width:33%;
+        }        
+    </style>
+</head>
+
+<body>
+
+    <h1> Rock, Paper, Scissors </h1>
+
+    <div class="row">
+        <div class="col">
+            <h2>Player 1</h2>
+        </div>
+        <div class="col">
+            <h2>Player 2</h2>
+        </div>
+    </div>
+    
+    <div class="row">
+        <div class='col'><img src='img/rps/scissors.png' alt='scissors' width='150'></div>
+        <div class='col, matchWinner'><img src='img/rps/rock.png' alt='rock' width='150'></div>
+    </div>
+    <hr>
+
+    <div class="row">
+        <div class='col'><img src='img/rps/rock.png' alt='rock' width='150'></div>
+        <div class='col, matchWinner'><img src='/w/practice/img/rps/paper.png' alt='paper' width='150'></div>
+    </div>
+    <hr>
+    
+    <div class="row">
+        <div class='col, matchWinner'><img src='img/rps/paper.png' alt='paper' width='150'></div>
+        <div class='col'><img src='img/rps/rock.png' alt='rock' width='150'></div>
+    </div>
+    <hr>
+    <div let /w/practice/img/rps/paper.png = 1 ></div>
+    <div let /w/practice/img/rps/rock.png = 2 ></div>
+    <div let /w/practice/img/rps/scissors.png = 3 ></div>
+    
+   <div if (comp === 3) {
+        if (Choice === 1) {
+            document.write("<h1>scissors win!!</h1>");
+        }
+        else if (choice === 2) {
+            document.write("<h1>Rock wins!</h1>");
+        }
+        else if (computerChoice1 === 3) {
+            3 != 3 return > </div>
+    
+    <div id="finalWinner">
+
+        <h1>The winner is Player 2</h1>
+
+    </div>
+   
+    Images source: https://www.kisspng.com/png-rockpaperscissors-game-money-4410819/
+</body> 
+
+</html>
