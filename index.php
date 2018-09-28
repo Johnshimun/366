b110ed6b66fe71582775c54aac288b7887900ad1
John Shimun <ymar-eshai-shimun@csumb.edu>
2018-09-28 21:02:51 +0000

    fixed index.php and added link to Lab 2

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-hello world
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+    </head>
+    <body>
+        <div>
+            <a href="./homework/777">Lab 2</a>
+        </div>
+
+    </body>
+</html>
