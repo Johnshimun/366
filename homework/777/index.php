258908347e66d8e929b5fd3e275aee2bfdc685ef
John Shimun <ymar-eshai-shimun@csumb.edu>
2018-09-29 08:08:08 +0000

    sending all to heroku, for better or worse

--- homework/777/index.php
+++ homework/777/index.php
@@ -1,3 +1,6 @@
+<?php
+include 'inc/functions.php';
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -5,63 +8,15 @@
         <style> @import url("css/styles.css"); </style>
     </head>
     <body>
-       
-<?php
-include 'inc/functions.php';
-
-displayPoints($randomValue1, $randomValue2, $randomValue3);
-
-function displaySymbol($randomValue, $pos)
-{
-    switch ($randomValue) {
-        case 0:
-            $symbol = "seven";
-            break;
-        case 1:
-            $symbol = "bar";
-            break;
-        case 2:
-            $symbol = "cherry";
-            break;
-    }
-    
-    echo "<img id ='reel$pos' src ='img/$symbol.png' alt='$symbol' title = '" . ucfirst($symbol) . "'width ='70'> ";
-}
-function displayPoints($randomValue1, $randomValue2, $randomValue3)
-{
-    echo "<div id = 'output'>";
-    
-    if ($randomValue1 == $randomValue2 && $randomValue2 == $randomValue3)  {
-        switch ($randomValue1) {
-            case 0:
-                $totalPoints = 1000;
-                echo "<h1> Jackpot! </h1>";
-                break;
-            case 1:
-                $totalPoints = 900;
-                break;
-            case 2:
-                $totalPoints = 500;
-                break;
-        }
-        
-        echo "<h2> You won $totalPoints points! </h2>";
-    } else {
-        echo "<h3> Try Again! </h3>";
-    }
-    echo "</div>";
-}
-
-
-?>
- <div id = "main">
+    <div id = "main">
         <?php
-play();
-?> 
+            play();
+        ?> 
         
         <form>
             <input type = "submit" value = "Spin!" />
         </form>
     </div>
-  </body>
+        
+    </body>
 </html>
