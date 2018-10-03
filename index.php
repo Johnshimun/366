3af0adabe531dcbb81eb9a14a35e260afa64015c
John Shimun <ymar-eshai-shimun@csumb.edu>
2018-10-03 07:05:23 +0000

    uploading heroku link

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
             <a href="./homework/777/index.php">Lab 2</a>
             <a href="./homework/1assignment/index.php">assign1</a>
             <a href="./homework/Homework1/index.html">hmwk1</a>
+            <a href="./homework/Slider/index.php">Carousel</a>
         </div>
 
     </body>
