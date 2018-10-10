3e45c2901f8a1057aa1fcbb34000b2e32d9a57b0
John Shimun <ymar-eshai-shimun@csumb.edu>
2018-10-10 16:58:12 +0000

    updating index

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
             <a href="./homework/1assignment/index.php">assign1</a>
             <a href="./homework/Homework1/index.html">hmwk1</a>
             <a href="./homework/Slider/index.php">Carousel</a>
+            <a href="./labs/smartcart/scart/index.php">scart</a>
         </div>
 
     </body>
