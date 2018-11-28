16fb5749e5a1fddc9be451285208bff9ddc5315c
John Shimun <ymar-eshai-shimun@csumb.edu>
2018-11-28 04:47:32 +0000

    uploading files

---
+++ earthquake/signup.js
@@ -0,0 +1,30 @@
+
+
+
+    function start (){
+        document.getElementById('introScreen').style.display = `none`;
+        clockTimer = window.setInterval(updateClock, 1000);
+    }
+    			
+    
+    var city;
+    var countu; 
+    
+    // code here can use carName
+    
+    function init() {
+        correctAnswer = document.getElementById(correctAnswer);
+    
+    function zipcode(){
+        if(zipcode==null){
+             testScreen.innerHTML = `<h1>no such zipcode</h1>`;
+        }
+        else{
+           // display= getCity;
+           //display = getcounty;
+        }
+        
+    }
+    
+}       
+        
